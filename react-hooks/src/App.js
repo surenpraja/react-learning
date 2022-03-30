9640215f3c85da60d9c05292794c7b10ef1983f2
surenpraja <praja.sunny.31@gmail.com>
2022-03-30 11:30:19 -0500

    doc: add odc document

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react'
 import ClassCounter from './components/ClassCounter';
 import DataFetching from './components/DataFetching';
 import HookCounter from './components/HookCounter';
@@ -9,11 +10,23 @@ import HookCounterTwo from './components/HookCounterTwo';
 import HookMouse from './components/HookMouse';
 import IntervalHookCounter from './components/IntervalHookCounter';
 import IntervalHookCounterPrac from './components/IntervalHookCounterPrac';
+import ComponentC from './components/useContextFolder/ComponentC';
+import UseReducer from './components/UseReducer';
+
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
 
 function App() {
   return (
     <div className="App">
-      <DataFetching/>
+      <UseReducer/>
+      {/* <UserContext.Provider value={"Outer layer: Vishwas"}>
+        <ChannelContext.Provider value = {"inner layer: Codevolution"}>
+          <ComponentC/>
+        </ChannelContext.Provider>
+      </UserContext.Provider> */}
+
+      {/* <DataFetching/> */}
       {/* <IntervalHookCounterPrac/>
       <IntervalHookCounter/> */}
       {/* <HookMouse/> */}
---
+++ react-hooks/src/components/UseReducer.js
@@ -0,0 +1,29 @@
+import React, {useReducer} from 'react'
+
+function UseReducer() {
+    const initialState = 0
+
+    const reducer = (state, action) =>
+    {
+        switch(action){
+            case "Increment":
+                return state + 1
+            case "Decrement":
+                return state - 1
+            case "Reset":
+                return initialState
+        }
+    }
+
+    const [count, dispatch] = useReducer(reducer, initialState)
+  return (
+    <div>
+        <div>Count:  {count} </div>
+        <button onClick={()=>dispatch("Increment")}>Increment</button>
+        <button onClick={()=>dispatch("Decrement")}>Decrement</button>
+        <button onClick={()=>dispatch("Reset")}>Reset</button>
+    </div>
+  )
+}
+
+export default UseReducer
